59f1772576b9eff74373dc344221ffc322d4967c
MarkShawn2020 <877210964@qq.com>
2021-04-22 10:12:19 +0800

    ++ update

--- auto_update_version.mark.sh
+++ auto_update_version.mark.sh
@@ -11,20 +11,22 @@ pcf=".git/hooks/post-commit"
 if [[ -f $pcf ]]
 then 
 {
-  echo "./git/hooks/post-commit文件已存在，请删除后再运行（谨慎）"
+  echo "${pcf}文件已存在，请删除后再运行（谨慎）"
   exit 1
 }
 fi
 
+echo "正在安装脚本……"
 
-cat > $pcf << EOF
+
+cat << 'EOF' >> $pcf
 commit=$(git show --no-patch --format=%B)
 echo "最新一次提交：$commit"
 
 if [[ $commit =~ ^\+\+\+ ]]
 then
 {
-  vt=3;
+  vt=3
   vm="重磅版本升级！！！"
 }
 else
@@ -32,12 +34,12 @@ else
   if [[ $commit =~ ^\+\+ ]]
   then
   {
-    vt=2;
+    vt=2
     vm="里程碑版本！"
   }
   else
     {
-      vt=1;
+      vt=1
       vm="版本优化~"
     }
   fi
@@ -51,17 +53,15 @@ echo "版本更新类型：$vm"
 
 vf=VERSION.txt
 
-if [[ ! -f "$vf" ]]
+if [[ ! -f $vf ]]
 then
 {
   echo "当前环境不存在版本文件：$vf"
   echo "正在新建……"
-  echo "0.0.0" >> "${vf}"
-commit="++ MVP"
+  echo "0.0.0" >> $vf
   echo "新建成功！"
 }
 else
-commit="++ MVP"
 {
   echo "正在读取版本文件：$vf"
 }
@@ -69,15 +69,12 @@ fi
 
 
 echo "当前版本：$(head -1 $vf)"
-commit="++ MVP"
-
 echo "正在尝试更新……"
 
 if [[ vt -eq 3 ]]
 then
 perl -pi -pe 's/(\d+)\.(\d+)\.(\d+)/($1+1).".$2.$3"/e' ${vf}
 else
-commit="++ MVP"
 if [[ vt -eq 2 ]]
 then
 perl -pi -pe 's/(\d+)\.(\d+)\.(\d+)/"$1.".($2+1).".$3"/e' ${vf}
@@ -85,7 +82,6 @@ else
 perl -pi -pe 's/(\d+)\.(\d+)\.(\d+)/"$1.$2.".($3+1)/e' ${vf}
 fi
 fi
-commit="++ MVP"
 
 echo "更新成功！"
 echo "更新后版本：$(head -1 $vf)"
